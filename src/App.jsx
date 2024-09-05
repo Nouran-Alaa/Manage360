144add4bd3e3d100f3c84a1c030914751cdc8e52
Nouran-Alaa <nouran180908@feng.bu.edu.eg>
2024-09-05 15:28:01 +0300

    update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./ui/AppLayout";
-import Login from "./features/user/Login";
-import Signup from "./features/user/Signup";
-import Profile from "./features/user/Profile";
 
 function App() {
   return (
@@ -10,9 +7,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="*" element={<AppLayout />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
